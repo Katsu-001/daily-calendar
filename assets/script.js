6fb8a5098b73efc079162f070b5f294dce673f8a
Katsuyuki <katsuyuki.russell@gmail.com>
2023-04-10 20:51:32 -0500

    final js update, adding click event and local storage

--- assets/script.js
+++ assets/script.js
@@ -11,25 +11,42 @@ $(document).ready(function () {
 
   createCalendar();
   updateBlockColor();
+  loadSavedEvents();
+
 
   // Updates clock
   setInterval(updateClock, 1000);
 
   // save button click event listener
   $(".saveBtn").on("click", function () {
+    let calendarBlock = $(this).parent().attr("id");
 
+    // if empty, remove the event from local storage
+
+
+    if ($(this).parent().find("textarea").val() == "") {
+      localStorage.removeItem(calendarBlock);
+      return;
+      // else if the text area is unchanged, do nothing
+    } else if ($(this).parent().find("textarea").val() == localStorage.getItem(calendarBlock)) {
+      return;
+      // otherwise, save event
+    } else {
+      localStorage.setItem(calendarBlock, $(this).parent().find("textarea").val());
+      window.alert("Event saved!");
+    }
   });
 
 
-  // displays ctime in header
+  // displays time in header
   function updateClock() {
-    // Sets current time in header
+    // sets current time in header
     let currentTime = dayjs().format("MMMM D YYYY, h:mm:ss a");
     $("#currentTime").html(currentTime);
 
-    // Gather current hour, for comparison to the displayed hour
+    // gather current hour, for comparison to the displayed hour
     currentHour = dayjs().format("H");
-    // Updates displayed hour if hour has changed
+    // updates displayed hour if hour has changed
     if (currentHour != displayedHour) {
       displayedHour = currentHour;
       updateBlockColor();
@@ -73,10 +90,10 @@ $(document).ready(function () {
   // changes color of the block given the time
   function updateBlockColor() {
     $(".time-block").each(function () {
-      // Get the hour from the id of the time block
+      // get the hour from the id of the time block
       let calendarBlockHour = parseInt($(this).attr("id").split("-")[1]);
 
-      // Sets Past, Present, or Future class
+      // sets Past, Present, or Future class
       if (calendarBlockHour < currentHour) {
         $(this).addClass("past");
         $(this).removeClass("present future");
@@ -91,8 +108,15 @@ $(document).ready(function () {
   }
 
   // retrieves local stored data
-  // function loadSavedEvents()
-
+  function loadSavedEvents() {
+    $(".time-block").each(function () {
+      let calendarBlock = $(this).attr("id");
+      let savedEvent = localStorage.getItem(calendarBlock);
+      if (savedEvent) {
+        $(this).find("textarea").val(savedEvent);
+      }
+    });
+  }
 });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
